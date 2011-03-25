56942f6070abf3bf73ad125f662f938250ec0633
Andreas-Christoph Bernstein <andreas.bernstein@googlemail.com>
2011-03-25 21:41:53 +0100

    added colorize function

--- mandelbrot.glsl
+++ mandelbrot.glsl
@@ -13,10 +13,41 @@ uniform vec2 resolution;
 varying vec2 tc;
 
 const float pi = 3.14159265358979323846264;
-const float iterations = 30;
-vec4 insideColor   = vec4(0.0, 0.0, 0.0,1.0);
+const float maxN  = 50;
+const float maxR2 = 100000.0;
+
+vec4 black   = vec4(0.0, 0.0, 0.0,1.0);
 vec4 outsideColor0 = vec4(0.01, 0.0, 0.9,1.0);
 vec4 outsideColor1 = vec4(0.8, 0.9, 0.0,1.0);
+
+//  z_n+1 = z_n + c, with z_0 = 0
+vec2
+iterate(in vec2 p) //, in float maxR2, in float N)
+{
+  vec2  c = p;
+  vec2  z = vec2(0.0);
+  float r2 = 0.0;
+	int n=0;
+  for (int i = 0 ; i < maxN && r2 < maxR2; ++i ) {
+    z  = complexSquare(z) + c;
+    r2 = dot(z, z);
+    n = i;
+  }
+  //float res = float(n) - log(log(r2) / log(4.0)) / log(2.0);
+  float res = float(n) - log2(0.5 * log(r2));
+  return vec2(res,r2);
+}
+
+vec4
+colorize(float n, float modSquared)
+{
+  vec4 col = black;
+  if (modSquared > 4.0)
+    col = mix(outsideColor0, outsideColor1, fract(n * 0.05));
+  //  col = vec4(hsvToRgb(vec3(modSquared, 1.0, n*0.09)), 1.0);
+  //  col = vec4(0.0, modSquared*0.001, fract(n*0.5), 1.0);
+  return col;
+}
 
 void
 main(void)
@@ -25,23 +56,7 @@ main(void)
   float s = sin(T) + 1.0;
 
   vec2 p = s * tc - vec2(1.40,0.02);
+  vec2 r = iterate(p);
 
-  vec4 col = insideColor;
-  float r2 = 0.0;
-
-  //  zn+1 = zn2 + c, with z0 = 0
-  vec2  c = p;
-  vec2  z = c; // vec2(0.0);
-  int iter = 0;
-  for (iter = 0 ; iter < iterations && r2 < 4.0; ++iter ) {
-    z  = complexSquare(z) + c;
-    r2 = dot(z, z);
-  }
-
-  if (r2 > 4.0)
-    col = mix(outsideColor0, outsideColor1, fract(float(iter) * 0.05));
-
-  // col = vec4(hsvToRgb(vec3(z.x, 0.7, z.y)), 1.0);
-
-  gl_FragColor = col;
+  gl_FragColor = colorize(r.x, r.y);
 }
