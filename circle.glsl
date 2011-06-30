f77bbf486c57a279569507e3dc8d2863d8ef0d5f
Andreas-Christoph Bernstein <andreas.bernstein@googlemail.com>
2011-07-01 00:28:53 +0200

    more circles

--- circle.glsl
+++ circle.glsl
@@ -5,12 +5,13 @@ uniform vec2 resolution;
 
 smooth in vec2 tc;
 out vec4 fragColor;
-const float rIn = 0.6;
-const float rOut = 0.7;
+const float rIn = 0.05;
+const float rOut = 0.1;
 
 void main()
 {
-	float d = distance(tc, vec2(0.0));
+	//float d = distance(tc, vec2(0.0));
+	float d = distance(mod(tc,vec2(0.5,1.0)), vec2(0.25,0.5));
 	if( d > rOut || d < rIn)
 		discard;
 	fragColor = vec4(1.0);
