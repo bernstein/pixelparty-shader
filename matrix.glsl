9e8b6dd098c4ed28a43f71c3ef32e095e766a1e4
Andreas-Christoph Bernstein <andreas.bernstein@uni-weimar.de>
2011-07-11 22:38:31 +0200

    column major in glsl

--- matrix.glsl
+++ matrix.glsl
@@ -15,10 +15,11 @@ rotX(in float a)
 {
   float c = cos(a);
   float s = sin(a);
-  return mat4(1.0, 0.0, 0.0, 0.0,
+  return transpose(
+          mat4(1.0, 0.0, 0.0, 0.0,
               0.0,   c,   s, 0.0,
               0.0,  -s,   c, 0.0,
-              0.0, 0.0, 0.0, 1.0 );
+              0.0, 0.0, 0.0, 1.0 ));
 }
 
 mat4
@@ -26,10 +27,11 @@ rotY(in float a)
 {
   float c = cos(a);
   float s = sin(a);
-  return mat4(  c, 0.0,  -s, 0.0,
+  return transpose(
+        mat4(  c, 0.0,  -s, 0.0,
               0.0, 1.0, 0.0, 0.0,
                 s, 0.0,   c, 0.0,
-              0.0, 0.0, 0.0, 1.0 );
+              0.0, 0.0, 0.0, 1.0 ));
 }
 
 mat4
@@ -37,10 +39,11 @@ rotZ(in float a)
 {
   float c = cos(a);
   float s = sin(a);
-  return mat4(  c,  -s, 0.0, 0.0,
+  return transpose(
+        mat4(  c,  -s, 0.0, 0.0,
                 s,   c, 0.0, 0.0,
               0.0, 0.0, 1.0, 0.0,
-              0.0, 0.0, 0.0, 1.0 );
+              0.0, 0.0, 0.0, 1.0 ));
 }
 
 mat4
@@ -49,10 +52,11 @@ from_frustum(in float l, in float r, in float t, in float b, in float n, in floa
   float r_lInv = 1.0 / (r - l);
   float t_bInv = 1.0 / (t - b);
   float f_nInv = 1.0 / (f - n);
-  return mat4(2.0*r_lInv, 0.0, (r+l)*r_lInv, 0.0,
+  return transpose(
+                mat4(2.0*r_lInv, 0.0, (r+l)*r_lInv, 0.0,
                      0.0, 2.0*n*t_bInv, (t+b)*t_bInv, 0.0,
                      0.0,      0.0, -(f+n)*f_nInv, -2*f*n*f_nInv,
-                     0.0,      0.0,         -1.0,          0.0);
+                     0.0,      0.0,         -1.0,          0.0));
 }
 
 mat4
@@ -62,10 +66,11 @@ from_symmetric_frustum(in float l, in float b, in float n, in float f)
   float t = -b;
 
   float f_nInv = 1 / (f - n);
-  return mat4( n/r, 0.0,0.0,0.0, 
+  return transpose(
+          mat4( n/r, 0.0,0.0,0.0, 
                0.0, n/t, 0.0, 0.0,
                0.0, 0.0, -(f+n)*f_nInv, -2*f*n*f_nInv,
-               0.0, 0.0, -1.0, 0.0);
+               0.0, 0.0, -1.0, 0.0));
 }
 
 mat4 
@@ -83,8 +88,9 @@ perspective(in float fov, in float aspect, in float n, in float f)
 mat4
 make_translate(vec3 tx)
 {
-  return mat4(1.0, 0.0, 0.0, tx.x,
+  return transpose(
+          mat4(1.0, 0.0, 0.0, tx.x,
               0.0, 1.0, 0.0, tx.y,
               0.0, 0.0, 1.0, tx.z,
-              0.0, 0.0, 0.0, 1.0);
+              0.0, 0.0, 0.0, 1.0));
 }
