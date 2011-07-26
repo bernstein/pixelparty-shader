08129c016d1c20c73c754cea74ab26340d2b1630
Andreas-Christoph Bernstein <andreas.bernstein@googlemail.com>
2011-07-26 21:46:19 +0200

    removed comment

--- complex_plane.glsl
+++ complex_plane.glsl
@@ -7,16 +7,10 @@ const float pi = 3.14159265358979323846264;
 smooth in vec2 tc;
 out vec4 fragColor;
 
-float
-argTesting(in vec2 z)
-{
-  return atan(z.y,z.x);
-}
-
 void 
 main(void)
 {
-  vec2 z = tc; // polar(tc);
+  vec2 z = tc;
   vec3 col = hsvToRgb(vec3(argTesting(z)/(2.0*pi), 0.7, magnitude(z)));
   fragColor = vec4(col, 1.0);
 }
