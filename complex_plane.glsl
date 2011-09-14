2be6e013f39984bd79727678c710af6df32c9bde
Andreas-Christoph Bernstein <andreas.bernstein@googlemail.com>
2011-09-14 19:37:19 +0200

    more experiments

--- complex_plane.glsl
+++ complex_plane.glsl
@@ -7,10 +7,26 @@ const float pi = 3.14159265358979323846264;
 smooth in vec2 tc;
 out vec4 fragColor;
 
+const vec4 white = vec4(1.0,1.0,1.0,1.0);
+const vec4 black = vec4(0.0,0.0,0.0,1.0);
+
+bool
+grid(in vec2 p)
+{
+  vec2 q = mod(abs(p),0.25);
+  return q.x < 0.01 || q.y < 0.01;
+}
+
+vec4
+bwIm(in bool reg)
+{
+  return reg ? white : black;
+}
+
 void 
 main(void)
 {
-  float zoom = 3.0;
+  float zoom = 2.0;
   vec2 z = zoom*tc;
   //vec2 q = col(z);
   //vec2 q = col(complexInvert(z));
@@ -19,7 +35,13 @@ main(void)
   //vec2 q = complexInvert(z+vec2(1.0,0.0)) - complexInvert(z - vec2(1.0,0.0));
   //vec2 q = complexInvert(z+vec2(1.0,0.0)) - complexInvert(z - vec2(1.0,0.0));
   vec2 q = complexInvert(z+vec2(1.0,0.0)) + complexInvert(z - vec2(1.0,0.0));
+  //vec2 q = complexExp(complexInvert(z));
+  //vec2 q = complexExp(complexInvert(complexSquare(z)));
+  //vec2 q = complexSin(z);
+  //vec2 q = complexSin(complexInvert(z));
   vec3 col = col(q);
   fragColor = vec4(col, 1.0);
-}
 
+  //vec4 col2 = bwIm(grid(q));
+  //fragColor = col2;
+}
